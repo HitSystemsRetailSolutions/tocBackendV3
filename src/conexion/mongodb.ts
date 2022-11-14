8f6a0d0b4dcd7491ca4928f60697a3940ff21e54
TecnicHIT <60508012+TecnicHIT@users.noreply.github.com>
2022-11-14 17:36:26 +0100

    Update mongodb.ts

--- src/conexion/mongodb.ts
+++ src/conexion/mongodb.ts
@@ -1,6 +1,6 @@
 import {MongoClient} from 'mongodb';
 
-const uri = 'mongodb://192.168.1.57:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+const uri = 'mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
 
 const client = new MongoClient(uri);
 const conexion = client.connect();
