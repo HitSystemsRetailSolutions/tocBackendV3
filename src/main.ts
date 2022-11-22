fde331dfc73815143959b53201ec4c04c576f00b
TecnicHIT <tecnic@hitsystems.es>
2022-11-22 16:02:33 +0100

    Build frontend Iterum

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   // app.enableCors();
   //await app.listen(3000);
   // await app.listen(3000,"10.137.0.201"); //para iterum ubuntu
-  //await app.listen(3000,"10.137.0.243"); //para iterum windows
-   await app.listen(3000,"192.168.1.57"); //para tpv pruebas
+  await app.listen(3000,"10.137.0.243"); //para iterum windows
+   //await app.listen(3000,"192.168.1.57"); //para tpv pruebas
 }
 bootstrap();
