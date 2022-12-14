407b90ebed5f7e0262a418400c513051548f7ce1
TecnicHIT <tecnic@hitsystems.es>
2022-12-14 16:19:36 +0100

    Prueba docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:16-slim
+FROM node:16
 
 RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
 
@@ -7,6 +7,11 @@ WORKDIR /home/node/app
 COPY package*.json ./
 
 RUN npm install
+
 COPY . .
+
+RUN npm run build
+
 EXPOSE 3000
+
 CMD [ "npm", "start" ]
--- src/conexion/mongodb.ts
+++ src/conexion/mongodb.ts
@@ -1,7 +1,11 @@
 import {MongoClient} from 'mongodb';
 
-const uri = 'mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
-
+//const uri = 'mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+//const uri = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+const uri = 'mongodb://mongodb:27017'; //?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+console.log(uri);
 const client = new MongoClient(uri);
+console.log(client);
 const conexion = client.connect();
+console.log(conexion);
 export {conexion};
